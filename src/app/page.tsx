79e934cb7044486e97e90e02f18848f2abb731c8
Lucas Pichette <lucascpichette@gmail.com>
2025-01-23 21:00:26 -0800

    updates search component to add command support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,11 +38,11 @@ export default function Home() {
 
     return (
         <div className="space-y-6">
+            <Search />
             <div className="flex justify-between items-center">
                 <h1 className="text-3xl font-bold">Trending</h1>
                 <TestStateButton />
             </div>
-            <Search />
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 <Suspense fallback={<div>Loading..</div>}>
                     {data &&
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -11,7 +11,7 @@ import {
     CommandSeparator,
     CommandShortcut,
 } from "@/components/ui/command";
-import { Suspense, useEffect } from "react";
+import { Suspense, useEffect, useRef } from "react";
 import { useState } from "react";
 import { Post } from "@/app/api/posts/route";
 import { useRouter } from "next/navigation";
@@ -50,7 +50,7 @@ function PostCommands({ data, setData }: Props) {
                         className="cursor-pointer"
                     >
                         <span>{post.title}</span>
-                        <CommandShortcut>⌘P</CommandShortcut>
+                        <CommandShortcut>⌘{post.id}</CommandShortcut>
                     </CommandItem>
                 ))}
         </CommandGroup>
@@ -60,39 +60,85 @@ function PostCommands({ data, setData }: Props) {
 export function Search() {
     const [isOpen, setIsOpen] = useState(false);
     const [data, setData] = useState<Post[] | null>(null);
+    const commandRef = useRef<HTMLDivElement>(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleKeyPress = (event: KeyboardEvent) => {
+            if (event.key === "Escape") {
+                setIsOpen(false);
+                return;
+            } else if (event.metaKey && data) {
+                const keyPressed = event.key;
+                const post = data.find((p) => p.id.toString() === keyPressed);
+                if (post) {
+                    router.push(`/entity/${post.id.toString()}`);
+                }
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyPress);
+        return () => {
+            window.removeEventListener("keydown", handleKeyPress);
+        };
+    }, [data, router]);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                commandRef.current &&
+                !commandRef.current.contains(event.target as Node)
+            ) {
+                setIsOpen(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
     return (
-        <Command className="rounded-lg border shadow-md transition-[height] duration-200 h-fit">
-            <CommandInput
-                placeholder="Type a command or search..."
-                onFocus={() => setIsOpen(true)}
-            />
-            {isOpen && (
-                <CommandList>
-                    <CommandEmpty>No results found.</CommandEmpty>
-                    <Suspense fallback={<></>}>
-                        <PostCommands data={data} setData={setData} />
-                        <CommandSeparator />
-                    </Suspense>
-                    <CommandGroup heading="Settings">
-                        <CommandItem>
-                            <User />
-                            <span>Profile</span>
-                            <CommandShortcut>⌘P</CommandShortcut>
-                        </CommandItem>
-                        <CommandItem>
-                            <CreditCard />
-                            <span>Billing</span>
-                            <CommandShortcut>⌘B</CommandShortcut>
-                        </CommandItem>
-                        <CommandItem>
-                            <Settings />
-                            <span>Settings</span>
-                            <CommandShortcut>⌘S</CommandShortcut>
-                        </CommandItem>
-                    </CommandGroup>
-                </CommandList>
-            )}
-        </Command>
+        <div className="relative w-full">
+            <div className="h-12 w-full"></div>
+            <div className="absolute top-0 left-0 w-full">
+                <Command
+                    ref={commandRef}
+                    className="rounded-lg border shadow-md transition-[height] duration-200 h-fit"
+                >
+                    <CommandInput
+                        placeholder="Type a command or search..."
+                        onFocus={() => setIsOpen(true)}
+                    />
+                    {isOpen && (
+                        <CommandList>
+                            <CommandEmpty>No results found.</CommandEmpty>
+                            <Suspense fallback={<></>}>
+                                <PostCommands data={data} setData={setData} />
+                                <CommandSeparator />
+                            </Suspense>
+                            <CommandGroup heading="Settings">
+                                <CommandItem>
+                                    <User />
+                                    <span>Profile</span>
+                                    <CommandShortcut>⌘P</CommandShortcut>
+                                </CommandItem>
+                                <CommandItem>
+                                    <CreditCard />
+                                    <span>Billing</span>
+                                    <CommandShortcut>⌘B</CommandShortcut>
+                                </CommandItem>
+                                <CommandItem>
+                                    <Settings />
+                                    <span>Settings</span>
+                                    <CommandShortcut>⌘S</CommandShortcut>
+                                </CommandItem>
+                            </CommandGroup>
+                        </CommandList>
+                    )}
+                </Command>
+            </div>
+        </div>
     );
 }
